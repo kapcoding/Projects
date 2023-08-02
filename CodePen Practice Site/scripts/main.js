6529116ee6be87b27c4202ddbec784e35d8dc9b3
kapcoding <141075211+kapcoding@users.noreply.github.com>
2023-08-02 12:26:10 -0500

    testing
    testing

---
+++ CodePen Practice Site/scripts/main.js
@@ -0,0 +1,13 @@
+$("#myLinkedIn").click(function () {
+    window.open("https://www.linkedin.com/in/keithpotter");
+    
+  });
+  
+  
+  //this doesn't work, why? it works when I put it in html
+  
+  $("button").click(function () {
+         window.open("https://drive.google.com/file/d/1h_QyVX3XxWkRSuhYHBiVuHelE982_24C/view?usp=sharing");
+         });
+                                                     
+  $                                                                                                       
---
+++ CodePen Practice Site/scripts/visited-places.js
@@ -0,0 +1,4 @@
+const myPlacesArr = [
+    "https://images.unsplash.com/photo-1565352336881-dc2c1f7d2d5b?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3wzMjM4NDZ8MHwxfHJhbmRvbXx8fHx8fHx8fDE2OTA0MTc2NDV8&ixlib=rb-4.0.3&q=80&w=400",
+    "https://images.unsplash.com/photo-1526958977630-bc61b30a2009?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3wzMjM4NDZ8MHwxfHJhbmRvbXx8fHx8fHx8fDE2OTA0MTc3NDR8&ixlib=rb-4.0.3&q=80&w=400"
+  ];
